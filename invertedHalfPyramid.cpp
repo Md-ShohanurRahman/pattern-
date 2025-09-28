a937c3d4a5158fab492eb5c6efe66885007fe066
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-09-28 12:15:16 +0600

    add more pattern

--- invertedHalfPyramid.cpp
+++ invertedHalfPyramid.cpp
@@ -2,18 +2,17 @@
 using namespace std;
 
 int main() {
-    int n =5;
-    int num= 1;
+    int n =4;
     for (int i = 1; i <= n; i++) {
      
-        for(int j=1;j<=i;j++){
-
-            cout<<num<<" ";
-            num++;
-            
+        for(int j =1 ; j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j =1 ; j<=i;j++){
+            cout<<"*";
         }
         cout<<endl;
-       
     }
+
     return 0;
 }
