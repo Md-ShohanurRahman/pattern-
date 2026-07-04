406fa808a1f32390374cb45231eef7c15dcecf53
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2026-07-04 18:27:21 +0600

    try different way to solve the code like half pyramid , inverted star

--- halfPyramid.cpp
+++ halfPyramid.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main() {
     int n =4;
     for (int i = 1; i <= n; i++) {
+        //int k=1;
         for (int j = 1; j <= i; j++) {
-            cout << j;
+           cout << j<<" ";
+            //cout<<k++<<" ";
         }
         cout << endl;
     }
--- invertedStarPattern.cpp
+++ invertedStarPattern.cpp
@@ -2,13 +2,21 @@
 using namespace std;
 
 int main() {
-    int n =4;
+     int n =4;
+// approach 1 
     for (int i = 1; i <= 4; i++) {
         for (int j = i; j <=4; j++) {
             cout << "*";
         }
         cout << endl;
     }
+// approach 2 
+    for (int i = 1; i <= 4; i++) {
+            for (int j = 4; j >=i; j--) {
+            cout << "*";
+            }
+            cout << endl;
+        }
 
-    return 0;
-}
+        return 0;
+    }
